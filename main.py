e6a450d78274e590328262e3599968ca5d7202c6
Kai Fergerstrom <kaifergerstrom@gmail.com>
2019-12-21 17:52:02 -0500

    Added requirements file w/ main loop logic

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import json, pickle, math
+import json, pickle, math, os, time, datetime
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-
 
 def serialize_df(filename, output):  # Create and serialize dataframe using pickle
 
@@ -32,10 +33,10 @@ def serialize_df(filename, output):  # Create and serialize dataframe using pick
 
 def create_df(filename):  # Parse pickled dataframe to features of dataset
 	df_old = pickle.load(open(filename, 'rb'))
-	df = df_old[['Open','High','Low', 'Close', 'Volume']]
+	df = df_old[['Date','Open','High','Low', 'Close', 'Volume']]
 	df['HL_PCT'] = (df['High'] - df['Low']) / df['Close'] * 100.0  # High/Low percent change (volatility)
 	df['PCT_change'] = (df['Close'] - df['Open']) / df['Open'] * 100.0  # PCT Change (increase in day)
-	df = df[['Close', 'HL_PCT', 'PCT_change', 'Volume']]  # Isolate important rows for training
+	df = df[['Date','Close', 'HL_PCT', 'PCT_change', 'Volume']]  # Isolate important rows for training
 	return df
 
 def create_training_data(df, predictor_col, pct_train, day_shift):
@@ -44,7 +45,7 @@ def create_training_data(df, predictor_col, pct_train, day_shift):
 	df['label'] = df[predictor_col].shift(-day_shift)
 
 	# Prepare X training data
-	X = np.array(df.drop(['label'], 1))  # Features (everything except labels)
+	X = np.array(df.drop(['label', 'Date'], 1))  # Features (everything except labels)
 	X = preprocessing.scale(X)  # Scale and Normalize x values
 	X = X[:-day_shift]  # Rest of data (not shifted)
 	X_predict = X[-day_shift:]  # Empty columns to predict
@@ -60,11 +61,35 @@ def create_training_data(df, predictor_col, pct_train, day_shift):
 	return predictions, accuracy
 
 
-#serialize_df("json/gldSymbol.json", "data")
-df = create_df("data")
-predictions, accuracy = create_training_data(df, "Close", 0.2, 8)
+def display_predictions():
+	pass
 
-print(predictions)
+
+if __name__ == "__main__":
+
+	outputFile = "data"  # Name of output serialize file
+	dataPath = "json/gldSymbol.json"  # Path to json file for data parsing
+
+	if not os.path.isfile(outputFile):  # If the file does not exist
+		print("Serializing dataframe!")
+		serialize_df(dataPath, outputFile)  # Create serialized dataframe
+
+	df = create_df("data")  # Format dataframe
+	predictions, accuracy = create_training_data(df, "Close", 0.2, 8)  # Get predictions
+	
+	'''
+	print(predictions, df['Close'])
+	style.use('ggplot')
+	plt.plot(df['Close'])
+	plt.plot(predictions)
+	plt.legend(loc=4)
+	plt.xlabel('Date')
+	plt.ylabel('Price')
+	plt.show()
+	'''
+
+		
+
 
 
 
